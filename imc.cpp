5df9e6f2170ac612d8231adbfd4d279df104c478
Gabriel Bruschi <travistouchdown500@gmail.com>
2020-09-18 16:44:14 -0400

    Criação do primeiro arquivo

---
+++ imc.cpp
@@ -0,0 +1,20 @@
+int main()
+{
+    float peso;
+    float altura;
+    float imc;
+
+    cout<<"Insira o peso:";
+    cin>>peso
+    cout<<"Insira a altura:";
+    cin>>altura
+
+    imc = peso/(altura*altura);
+
+    if imc > 30
+    	("Peso acima do normal")
+    else
+    	("Peso normal")
+
+	return 0;
+}
